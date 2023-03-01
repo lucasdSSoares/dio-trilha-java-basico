70f390bba3d52f697066c78dff5d561c63323c67
lucasdSSoares <lucasssoares36@gmail.com>
2023-03-01 16:31:28 -0300

    Exercise proposed to create the simple functioning of a SMARTV

---
+++ SISTEMA-SMART-TV/src/Usuario.java
@@ -0,0 +1,54 @@
+import java.awt.image.SampleModel;
+
+public class Usuario {
+
+	public static void main(String[] args) {
+
+/* Exercise proposed to create the simple functioning of a SMARTV, where we can:
+ * - Turn On and off and check TV status; 
+ * - Increase and decrease the volume;
+ * - Change the channel
+ * 
+ * Exercício proposto para criarmos o funcionamento simples de uma SMARTV, onde podemos: 
+ * - Ligar e desligar e verificar o status da TV;
+ * - Aumentar e diminuir o volume e; 
+ * - Trocar de Canal
+ */
+		SmartTv smartTv = new SmartTv();		
+
+		System.out.println("****** Status inicial da SmatTV ****** ");
+		System.out.println("* Tv ligada...? " + smartTv.ligada);
+		System.out.println("* Canal atual.: " + smartTv.canal);
+		System.out.println("* Volume atual: " + smartTv.volume);
+		System.out.println("**************************************\n");
+		
+		smartTv.mudarCanal(13);			
+		smartTv.aumentarCanal();
+		smartTv.aumentarCanal();
+		smartTv.diminuirCanal();
+		
+		smartTv.ligar();
+		System.out.println("Novo Status.: " + smartTv.ligada);
+		
+		smartTv.desligar();
+		System.out.println("Novo Status.: " + smartTv.ligada);
+
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.diminuirVolume();
+		smartTv.aumentarVolume();
+		
+		System.out.println("Volume atual.: " + smartTv.volume);
+			
+		
+		System.out.println("\n****** Status final da SmatTV ****** ");
+		System.out.println("* Tv ligada...? " + smartTv.ligada);
+		System.out.println("* Canal atual.: " + smartTv.canal);
+		System.out.println("* Volume atual: " + smartTv.volume);
+		System.out.println("**************************************\n");
+		
+		
+		
+	}
+
+}
